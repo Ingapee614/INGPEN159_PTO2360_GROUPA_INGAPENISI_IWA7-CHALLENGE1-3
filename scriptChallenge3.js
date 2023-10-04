bbf5b1192c3a8ebb1068cf233a01af258fb38947
Ingapee614 <132918785+Ingapee614@users.noreply.github.com>
2023-10-04 19:25:18 +0200

    change from negattive to positive number

--- scriptChallenge3.js
+++ scriptChallenge3.js
@@ -11,11 +11,16 @@ const divider = '----------------------------------';
 
 // Only change below this line
 
-const owed = parseFloat(leoBalance) + parseFloat(sarahBalance); // Calculate the total owed as a float
-const leo = `${leoName} ${leoSurname} Owed: R${parseFloat(leoBalance).toFixed(2)}`;
-const sarah = `${sarahName} ${sarahSurname} Owed: R${parseFloat(sarahBalance).toFixed(2)}`;
+const owed = Math.abs(leoBalance) + Math.abs(sarahBalance); // change values to be positive
+const leo = `${leoName} ${leoSurname} Owed: R${Math.abs(leoBalance).toFixed(2)}`;
+const sarah = `${sarahName} ${sarahSurname} Owed: R${Math.abs(sarahBalance).toFixed(2)}`;
 const total = "Total amount owed: ";
-const result = leo + "\n" + sarah + "\n" + divider  + "\n" + total + "R" + owed.toFixed(2) + "\n" + divider;
+const convert = new Intl.NumberFormat('en-US').format(owed);
+
+const result = leo + "\n" + sarah + "\n" + divider  + "\n" + total + "R" + convert + "\n" + divider;
 
 console.log(result);
+  
+
+
 
