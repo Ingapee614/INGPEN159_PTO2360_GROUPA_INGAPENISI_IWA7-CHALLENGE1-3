5effd4d80697f496c8dc03d4ea5f1e8b07024582
Ingapee614 <132918785+Ingapee614@users.noreply.github.com>
2023-10-12 16:52:12 +0200

    EDITED

--- scriptChallenge1.js
+++ scriptChallenge1.js
@@ -1,2 +1,2 @@
-const value = 3; //declared as string instead of number
-console.log(value + 4 + value)
+const value = '3'; //declared as string instead of number
+console.log(parseInt(value) + 4 + parseInt(value));
